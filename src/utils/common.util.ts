8c1f0229f948319f50dd0a4ae4de5537b7d07110
Yang Chuan <realyangchuan@gmail.com>
2023-11-06 10:24:32 +0800

    nothing

--- src/utils/common.util.ts
+++ src/utils/common.util.ts
@@ -1,8 +1,7 @@
 /**
  * 暂停
  * @param time 暂停时间：ms
- * @returns Promise<undefined>
  */
 export function sleep(time: number) {
-  return new Promise(resolve => setTimeout(resolve, time))
+  return new Promise<undefined>(resolve => setTimeout(resolve, time))
 }
